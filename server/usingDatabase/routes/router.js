c383bee4ee23012c5ab57e26365003bc8262ca03
Viola <vvvviola@yahoo.com>
2019-03-16 05:13:36 +0100

    feat(user): implement login endpoint
    ensure credentials and missing fields are validated
    define endpoint and method for user login
    test with postman
    ensure spec tests for login route passed
    
    [Delivers #164595214]

--- server/usingDatabase/routes/router.js
+++ server/usingDatabase/routes/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Handle /api/v1 endpoint
 router.get('/', (req, res) => {
   res.status(200).send({
-    message: 'Welcome to EPIC-mail API version 1',
+    message: 'Welcome to EPIC-mail API version 2',
   });
 });
 
@@ -15,5 +15,9 @@ router.post('/auth/signup',
   UserValidator.validateSignUp,
   UserValidator.validateExistingUser,
   UserController.signUp);
+
+router.post('/auth/login',
+  UserValidator.validateSignIn,
+  UserController.signIn);
 
 export default router;
