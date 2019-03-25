b86d088f3e30f2e01dac17e9e3ec071b0baa0d2f
Viola <vvvviola@yahoo.com>
2019-03-25 17:04:15 +0100

    Swagger Documentation V2

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const messageContainer = document.querySelector('.message-container');
 const messageBoxes = document.querySelectorAll('.message-box');
 const threadBox = document.querySelector('.thread-box');
@@ -12,8 +13,8 @@ if (window.innerWidth <= 768) {
       threadBox.style.marginLeft = 0;
       boxBtn.style.display = 'block';
       chatForm.style.width = '100%';
-      chatForm.style.borderLeft = "5px solid #10a9f0";
-    })
+      chatForm.style.borderLeft = '5px solid #10a9f0';
+    });
   });
 }
 
@@ -22,15 +23,15 @@ boxBtn.addEventListener('click', event => {
   threadBox.style.display = 'none';
   boxBtn.style.display = 'none';
   messageContainer.style.width = '100%';
-})
+});
 
-const sidepanel = document.getElementById("sidepanel");
+const sidepanel = document.getElementById('sidepanel');
 
 const openNav = () => {
-  sidepanel.style.width = "250px";
-  sidepanel.style.position = "fixed";
+  sidepanel.style.width = '250px';
+  sidepanel.style.position = 'fixed';
 };
 
 const closeNav = () => {
-  sidepanel.style.width = "0";
+  sidepanel.style.width = '0';
 };
